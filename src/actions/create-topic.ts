63eece78e11386c287cc641c4f63615850fbb516
Kanan Farzali <kanan.farzali@gmail.com>
2024-05-19 23:35:00 +0200

    fixing useformstate type errors

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -12,7 +12,17 @@ const createTopicSchema = z.object({
   description: z.string().min(10),
 });
 
-export const createTopic = async (formData: FormData) => {
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+  };
+}
+
+export const createTopic = async (
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> => {
   const result = createTopicSchema.safeParse({
     name: formData.get('name'),
     description: formData.get('description'),
@@ -20,7 +30,14 @@ export const createTopic = async (formData: FormData) => {
 
   if (!result.success) {
     console.log(result.error.flatten().fieldErrors);
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
   }
+
+  return {
+    errors: {},
+  };
 
   // TODO: revalidate the home page
 };
