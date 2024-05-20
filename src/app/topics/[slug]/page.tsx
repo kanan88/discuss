4f1625cccfdf06f54eb7e4ee0da37365160ecffd
Kanan Farzali <kanan.farzali@gmail.com>
2024-05-20 21:28:29 +0200

    scaffolding topic show page

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,5 +1,18 @@
-const TopicShowPage = () => {
-  return <div>Topic Show Page</div>;
+interface TopicShowPageProps {
+  params: {
+    slug: string;
+  };
+}
+
+const TopicShowPage = ({ params }: TopicShowPageProps) => {
+  const { slug } = params;
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+      </div>
+    </div>
+  );
 };
 
 export default TopicShowPage;
